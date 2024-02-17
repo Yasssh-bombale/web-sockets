d0625a55e682d3870b2bae1a9625b2ba61e21b32
Yash Vijay Bombale <yashbombale@gmail.com>
2024-02-17 17:48:39 +0530

    connect sockets to both frontend and backend

--- socket_1/server/app.js
+++ socket_1/server/app.js
@@ -1,12 +1,31 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 const port = 8000;
 
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+}); //circuit
+
+io.on("connection", (socket) => {
+  console.log(`user connected`);
+  console.log(`Id ${socket.id}`);
+  socket.emit("welcome", `welcome to the server,${socket.id}`); //it will send msg to the particular circuit
+  // broadcast is used to send to other circuites;
+  socket.broadcast.emit("welcome", `${socket.id} has just joined the server`);
+});
 
 app.get("/", (req, res) => {
   res.send("homePage");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
